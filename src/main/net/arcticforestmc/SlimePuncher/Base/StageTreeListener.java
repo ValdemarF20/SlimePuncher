29727d806c1d7d6ad4cd783de383f5014472057b
Connor Michalec <connormichalec@gmail.com>
2021-03-20 12:06:26 -0500

    V0.0.0-ALPHA-SNAPSHOT merge work

--- src/main/net/arcticforestmc/SlimePuncher/Base/StageTreeListener.java
+++ src/main/net/arcticforestmc/SlimePuncher/Base/StageTreeListener.java
@@ -1,21 +1,13 @@
 package net.arcticforestmc.SlimePuncher.Base;
 
 import org.bukkit.Location;
-import org.bukkit.Sound;
-import org.bukkit.SoundCategory;
-import org.bukkit.World;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.EquipmentSlot;
 
 
 public class StageTreeListener implements Listener{
     private GamePlayer owner;
-    public Location slimeLocation;
 
     public StageTreeListener(GamePlayer owner){
         this.owner = owner;
@@ -23,17 +15,6 @@ public class StageTreeListener implements Listener{
 
     @EventHandler
     public void onInteract(PlayerInteractEvent e){
-        Player player = e.getPlayer();
-        slimeLocation = new Location(player.getWorld(), 0, 0, 0);   //Requires configuration
-        final Block clickedBlock = e.getClickedBlock();
-        Location blockLocation = clickedBlock.getLocation();
-
-        if (!(e.getAction() == Action.LEFT_CLICK_BLOCK) && !(e.getHand().equals(EquipmentSlot.HAND))){return;}
-
-        if (blockLocation == slimeLocation){
-            owner.addBits();
-            player.playSound(player.getLocation(), Sound.BLOCK_SLIME_STEP, SoundCategory.BLOCKS,10, 3);
-        }
         owner.getStageTree().stages.forEach((stage) -> stage.onInteractEvent(e));
     }
 }
--- src/main/net/arcticforestmc/SlimePuncher/Stages/Stage0_0_SlimePuncher.java
+++ src/main/net/arcticforestmc/SlimePuncher/Stages/Stage0_0_SlimePuncher.java
@@ -1,7 +1,14 @@
 package net.arcticforestmc.SlimePuncher.Stages;
 
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 
 import net.arcticforestmc.SlimePuncher.SlimePuncher;
 import net.arcticforestmc.SlimePuncher.Base.GamePlayer;
@@ -36,7 +43,18 @@ public class Stage0_0_SlimePuncher extends Stage {
 
     @Override
     public void onInteractEvent(PlayerInteractEvent e){
-        
+        Location slimeLocation;
+        Player player = e.getPlayer();
+        slimeLocation = new Location(player.getWorld(), 0, 0, 0);   //Requires configuration
+        final Block clickedBlock = e.getClickedBlock();
+        Location blockLocation = clickedBlock.getLocation();
+
+        if (!(e.getAction() == Action.LEFT_CLICK_BLOCK) && !(e.getHand().equals(EquipmentSlot.HAND))){return;}
+
+        if (blockLocation == slimeLocation){
+            owner.addBits();
+            player.playSound(player.getLocation(), Sound.BLOCK_SLIME_STEP, SoundCategory.BLOCKS,10, 3);
+        }
     }
     
 }
