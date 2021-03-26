3bac763eeb8b656f5cc34a6334527c71327b11c0
Connor Michalec <connormichalec@gmail.com>
2021-03-26 17:30:35 -0500

    V0.0.0-AlPHA-SNAPSHOT - Tests

--- src/test/net/arcticforestmc/SlimePuncher/StageTreeTest.java
+++ src/test/net/arcticforestmc/SlimePuncher/StageTreeTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import net.arcticforestmc.SlimePuncher.Base.GamePlayer;
 import net.arcticforestmc.SlimePuncher.Base.StageTree;
 import org.mockito.Mock;
 
@@ -13,4 +14,10 @@ public class StageTreeTest {
 
     @Mock
     SlimePuncher slimepuncher;
+    GamePlayer gameplayer;
+
+    @Test
+    public void test() {
+       System.out.println(new StageTree(slimepuncher, gameplayer).generateDebugTreeGraph());
+    }
 }
