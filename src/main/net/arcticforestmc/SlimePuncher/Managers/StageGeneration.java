4130b160934e8d0215c106e16fa614dd0d883379
Connor Michalec <connormichalec@gmail.com>
2021-04-10 15:29:07 -0500

    V0.0.0-ALPHA-SNAPSHOT - Inital Stage generation implementation

--- src/main/net/arcticforestmc/SlimePuncher/Managers/StageGeneration.java
+++ src/main/net/arcticforestmc/SlimePuncher/Managers/StageGeneration.java
@@ -5,17 +5,18 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.Vector;
 import com.sk89q.worldedit.WorldEditException;
-import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.bukkit.BukkitWorld;
 import com.sk89q.worldedit.extent.clipboard.Clipboard;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
-import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats;
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardReader;
 import com.sk89q.worldedit.function.operation.Operation;
 import com.sk89q.worldedit.function.operation.Operations;
-import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.session.ClipboardHolder;
+import com.sk89q.worldedit.world.registry.WorldData;
 
+import org.bukkit.entity.Slime;
 
 import net.arcticforestmc.SlimePuncher.SlimePuncher;
 import net.arcticforestmc.SlimePuncher.Base.GamePlayer;
@@ -26,30 +27,33 @@ public class StageGeneration {
     /** 
      * Generate next stage from schematic.
      */
-    public void generateStage(GamePlayer player, String stageIdentifier) {
+    public static void generateStage(GamePlayer player, String stageIdentifier, int x, int y, int z) {
         File schematicFile = new File("plugins/SlimePuncher/stageSchematics/"+stageIdentifier+".schematic");
-        ClipboardFormat format = ClipboardFormats.findByFile(schematicFile);
+        ClipboardFormat format = ClipboardFormat.findByFile(schematicFile);
 
         Clipboard clipboard = null;
         ClipboardReader reader = null;
 
+        com.sk89q.worldedit.world.World weWorld = new BukkitWorld(player.getOwner().getWorld());
+        WorldData weWorldData= weWorld.getWorldData();
+
         try {
             reader = format.getReader(new FileInputStream(schematicFile));
-            clipboard = reader.read();
+            clipboard = reader.read(weWorldData);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        com.sk89q.worldedit.world.World weWorld = BukkitAdapter.adapt(player.getOwner().getWorld());
+        
  
         EditSession editSession = SlimePuncher.worldEdit.getEditSessionFactory().getEditSession(weWorld, -1);
 
-        Operation operation = new ClipboardHolder(clipboard).createPaste(editSession)
-        .to(BlockVector3.at(0, 6, 0)).ignoreAirBlocks(true).build();
+
+        Operation operation = new ClipboardHolder(clipboard, weWorldData).createPaste(editSession, weWorldData)
+        .to(new Vector(x, y, z)).ignoreAirBlocks(true).build();
         
         try {
             Operations.complete(operation);
-            editSession.flushSession(); //lock in changes
         } catch (WorldEditException e) {
             e.printStackTrace();
         }
